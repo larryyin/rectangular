78caa6cb31797d09466456242e63464d41b4850c
Larry Yin <larryyin@hotmail.com>
2016-05-11 01:23:25 -0400

    Port changed to 7110; author and email added.

--- rectangular.py
+++ rectangular.py
@@ -547,5 +547,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-#    app.run(host= '0.0.0.0',debug=True)
-    app.run(debug=True)
+#    app.run(host= '0.0.0.0',port=7110,debug=True)
+    app.run(port=7110,debug=True)
